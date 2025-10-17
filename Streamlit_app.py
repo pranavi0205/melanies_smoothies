508922fc19e193484162fd4f2efae867bce067b6
pranavi0205 <157849127+pranavi0205@users.noreply.github.com>
2025-10-16 22:50:48 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
+pip install snowflake-connector-python
 from snowflake.snowpark.functions import col
 
 cnx=st.connection("snowflake")
