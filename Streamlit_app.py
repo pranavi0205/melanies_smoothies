98b49fbb75943e317e8e5016fee82f1a806429db
pranavi0205 <157849127+pranavi0205@users.noreply.github.com>
2025-10-16 20:51:15 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
 
 
