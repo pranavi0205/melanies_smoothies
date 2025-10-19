dbc1101c1da843c8bb77979adf6383397835b912
pranavi0205 <157849127+pranavi0205@users.noreply.github.com>
2025-10-19 11:24:27 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -24,6 +24,8 @@ st.write('The name on your Smoothie will be ',name_on_order)
 
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #my_dataframe(data=my_dataframe,use_container_width=True)
+st.dataframe(data=my_dataframe,use_container_width=True)
+st.stop()
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:',my_dataframe
