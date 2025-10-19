de2ec88e57608c8d451ee164d1e31b6323b9ba51
pranavi0205 <157849127+pranavi0205@users.noreply.github.com>
2025-10-19 11:47:18 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -31,9 +31,9 @@ ingredients_list=st.multiselect(
 
 #st.dataframe(data=my_dataframe,use_container_width=True)
 #st.stop()
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 if st.button('Submit'):
     if ingredients_list and name_on_order:
         ingredients_string = ', '.join(ingredients_list)
