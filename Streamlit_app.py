5bf5b7392a01e5bd24bf434e7de539eb2a8f6f52
pranavi0205 <157849127+pranavi0205@users.noreply.github.com>
2025-10-19 11:41:22 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -23,8 +23,6 @@ st.write('The name on your Smoothie will be ',name_on_order)
 
 
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 #st.dataframe(data=my_dataframe,use_container_width=True)
 #st.stop()
 pd_df=my_dataframe.to_pandas()
